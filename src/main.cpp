7b45495acbbd10cbafd4f2e2a2739aaba7cc2627
Rbel12b <rbel12b@gmail.com>
2025-04-15 14:46:43 +0200

    imgui with SDL2 renderer, empty window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,89 @@
-#include <iostream>
+/* main.cpp */
+#include "imgui.h"
+#include "backends/imgui_impl_sdl2.h"
+#include "backends/imgui_impl_sdlrenderer2.h"
+#include <SDL2/SDL.h>
+#include <stdio.h>
 
-int main (int argc, char *argv[]) {
-    std::cout << "Hello, World!" << std::endl;
+int windowHeight, windowWidth;
+
+int main(int, char **)
+{
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
+    {
+        printf("Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Create window
+    SDL_Window *window = SDL_CreateWindow("Calculator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    windowWidth = 800;
+    windowHeight = 600;
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer bindings
+    ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer2_Init(renderer);
+
+    // Main loop
+    bool running = true;
+    SDL_Event event;
+
+    float a = 0.0f, b = 0.0f;
+    float result = 0.0f;
+
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED)
+            {
+                SDL_GetWindowSize(window, &windowWidth, &windowHeight);
+                SDL_RenderSetLogicalSize(renderer, windowWidth, windowHeight);
+            }
+        }
+
+        ImGui_ImplSDLRenderer2_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0);
+        ImGui::SetNextWindowSize(ImVec2(windowWidth, windowHeight));
+        ImGui::SetNextWindowPos(ImVec2(0, 0));
+        ImGui::Begin("Calculator", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar);
+        ImGui::End();
+        ImGui::PopStyleVar(1);
+
+        ImGui::Render();
+        SDL_SetRenderDrawColor(renderer, 114, 144, 154, 255);
+        SDL_RenderClear(renderer);
+        ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData(), renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    // Cleanup
+    ImGui_ImplSDLRenderer2_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
