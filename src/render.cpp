a7044bc40660944833aafb73b5ee529d594a5c21
Rbel12b <rbel12b@gmail.com>
2025-04-17 16:52:12 +0200

    add renderer class

---
+++ src/render.cpp
@@ -0,0 +1,84 @@
+#include "render.h"
+
+int Renderer::Init(const char* windowTitle)
+{
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
+    {
+        printf("Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Create window
+    window = SDL_CreateWindow(windowTitle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    windowWidth = 800;
+    windowHeight = 600;
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer bindings
+    ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer2_Init(renderer);
+    running = true;
+    return 0;
+}
+
+void Renderer::ProcessEvents()
+{
+    while (SDL_PollEvent(&event))
+    {
+        ImGui_ImplSDL2_ProcessEvent(&event);
+        if (event.type == SDL_QUIT)
+        {
+            running = false;
+        }
+        if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED)
+        {
+            SDL_GetWindowSize(window, &windowWidth, &windowHeight);
+            SDL_RenderSetLogicalSize(renderer, windowWidth, windowHeight);
+        }
+        if (m_eventCallback)
+        {
+            m_eventCallback(event);
+        }
+    }
+}
+
+void Renderer::Render()
+{
+    ImGui_ImplSDLRenderer2_NewFrame();
+    ImGui_ImplSDL2_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0);
+    ImGui::SetNextWindowSize(ImVec2(windowWidth, windowHeight));
+    ImGui::SetNextWindowPos(ImVec2(0, 0));
+    ImGui::Begin("main", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar);
+    ImGui::End();
+    ImGui::PopStyleVar(1);
+
+    ImGui::Render();
+    SDL_SetRenderDrawColor(renderer, 114, 144, 154, 255);
+    SDL_RenderClear(renderer);
+    ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData(), renderer);
+    SDL_RenderPresent(renderer);
+}
+
+void Renderer::Shutdown()
+{
+    ImGui_ImplSDLRenderer2_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    running = false;
+}
---
+++ src/render.h
@@ -0,0 +1,46 @@
+#pragma once
+#ifndef _RENDER_H_
+#define _RENDER_H_
+
+#include "imgui.h"
+#include "backends/imgui_impl_sdl2.h"
+#include "backends/imgui_impl_sdlrenderer2.h"
+#include <SDL2/SDL.h>
+
+class Renderer
+{
+public:
+    Renderer()
+    {
+
+    }
+    ~Renderer()
+    {
+        if (running)
+        {
+            Shutdown();
+        }
+    }
+
+    int Init(const char* windowTitle);
+    void Render();
+    void Shutdown();
+    void ProcessEvents();
+    bool IsRunning() const
+    {
+        return running;
+    }
+    void SetEventCallback(void (*callback)(SDL_Event &event))
+    {
+        m_eventCallback = callback;
+    }
+private:
+    bool running = false;
+    int windowHeight, windowWidth;
+    SDL_Event event;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    void (*m_eventCallback)(SDL_Event &event) = nullptr;
+};
+
+#endif
